9e526ebe9829d290e990af9935a147c227366d55
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2020-03-15 15:29:17 +0200

    [WIP] adding search - hiding search icon when in search screen; added searchview

--- app/src/main/java/com/theupnextapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/theupnextapp/ui/search/SearchFragment.kt
@@ -1,6 +1,74 @@
 package com.theupnextapp.ui.search
 
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.theupnextapp.databinding.FragmentSearchBinding
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import com.theupnextapp.R
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(),
+    OnQueryTextListener {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    private val viewModel: SearchViewModel by lazy {
+        val activity = requireNotNull(activity) {
+            "You can only access the viewModel after onActivityCreated"
+        }
+        ViewModelProviders.of(
+            this,
+            SearchViewModel.Factory(activity.application)
+        ).get(SearchViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(inflater)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.viewModel = viewModel
+
+        binding.search.setIconifiedByDefault(true)
+        binding.search.isFocusable = true
+        binding.search.isIconified = false
+        binding.search.requestFocusFromTouch()
+        binding.search.queryHint = "Start typing the show name here..."
+        binding.search.setOnQueryTextListener(this)
+
+        return binding.root
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        val menuItem  = menu.findItem(R.id.searchFragment)
+        menuItem.isVisible = false
+
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        viewModel.onQueryTextSubmit(query)
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        viewModel.onQueryTextChange(newText)
+        return true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity).supportActionBar?.title = "Show search"
+    }
 }
--- app/src/main/java/com/theupnextapp/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/theupnextapp/ui/search/SearchViewModel.kt
@@ -2,6 +2,26 @@ package com.theupnextapp.ui.search
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class SearchViewModel(application: Application) : AndroidViewModel(application) {
+
+    fun onQueryTextSubmit(query : String?){
+
+    }
+
+    fun onQueryTextChange(newText : String?) {
+
+    }
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(SearchViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return SearchViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewModel")
+        }
+    }
 }
