1e6ff4478d3c2d627b2ea8981f0917922bf0508e
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2020-06-03 22:10:49 +0200

    Added observers for invalid trakt token and invalid trakt grant

--- app/src/main/java/com/theupnextapp/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/theupnextapp/ui/history/HistoryFragment.kt
@@ -141,6 +141,28 @@ class HistoryFragment : BaseFragment(), HistoryAdapter.HistoryAdapterListener {
                 viewModel.displayShowDetailsComplete()
             }
         })
+
+        viewModel.invalidToken.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                Snackbar.make(
+                    binding.root,
+                    getString(R.string.trakt_invalid_token_response_received),
+                    Snackbar.LENGTH_LONG
+                ).show()
+                viewModel.onInvalidTokenResponseReceived(it)
+            }
+        })
+
+        viewModel.invalidGrant.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                Snackbar.make(
+                    binding.root,
+                    getString(R.string.trakt_invalid_grant_response_received),
+                    Snackbar.LENGTH_LONG
+                ).show()
+                viewModel.onInvalidTokenResponseReceived(it)
+            }
+        })
     }
 
     override fun onResume() {
