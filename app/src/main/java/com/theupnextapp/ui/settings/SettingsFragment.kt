4c8cd99b57685f4e525bc5a804e28e35ae488ca5
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2020-06-23 21:04:24 +0200

    [WIP] Addition of settings screen

---
+++ app/src/main/java/com/theupnextapp/ui/settings/SettingsFragment.kt
@@ -0,0 +1,31 @@
+package com.theupnextapp.ui.settings
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import com.theupnextapp.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        val settingsItem = menu.findItem(R.id.menu_settings)
+        if (settingsItem != null) {
+            settingsItem.isVisible = false
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.title_settings)
+    }
+}
