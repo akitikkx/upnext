4d2966f1385c8aea84e21754c2c69f1019390959
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2021-07-14 11:29:57 +0200

    Retrieval and storage of favorites into database and displaying on account screen

---
+++ app/src/main/java/com/theupnextapp/work/RefreshFavoriteShowsWorker.kt
@@ -0,0 +1,38 @@
+package com.theupnextapp.work
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.WorkerParameters
+import com.theupnextapp.repository.TraktRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.coroutineScope
+
+@HiltWorker
+class RefreshFavoriteShowsWorker @AssistedInject constructor(
+    @Assisted appContext: Context,
+    @Assisted workerParameters: WorkerParameters,
+    private val traktRepository: TraktRepository
+) : BaseWorker(appContext, workerParameters) {
+
+    override val contentTitle = "Refreshing your favorite shows"
+
+    override suspend fun doWork(): Result = coroutineScope {
+        try {
+            setForeground(createForegroundInfo())
+            val token = inputData.getString(ARG_TOKEN)
+            token?.let { refreshFavoriteShows(token = it, repository = traktRepository) }
+            Result.success()
+        } catch (e: Exception) {
+            Result.failure()
+        }
+    }
+
+    private suspend fun refreshFavoriteShows(token: String, repository: TraktRepository) {
+        repository.refreshFavoriteShows(token)
+    }
+
+    companion object {
+        const val ARG_TOKEN = "arg_token"
+    }
+}
