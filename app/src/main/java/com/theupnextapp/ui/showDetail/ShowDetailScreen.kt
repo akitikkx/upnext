5405d0af9898d4baadd52b819c8ab753c41e222c
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2022-01-21 17:40:18 +0200

    [WIP] Updating the show detail screen to be a composable screen

--- app/src/main/java/com/theupnextapp/ui/showDetail/ShowDetailScreen.kt
+++ app/src/main/java/com/theupnextapp/ui/showDetail/ShowDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.theupnextapp.ui.showDetail
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,8 +8,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -23,11 +28,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.theupnextapp.R
+import com.theupnextapp.domain.ShowCast
+import com.theupnextapp.domain.ShowDetailArg
+import com.theupnextapp.domain.ShowDetailSummary
 import com.theupnextapp.ui.components.PosterImage
+import org.jsoup.Jsoup
 
 @Composable
 fun ShowDetailScreen(viewModel: ShowDetailViewModel = hiltViewModel()) {
     val showSummary = viewModel.showSummary.observeAsState()
+
+    val showCast = viewModel.showCast.observeAsState()
 
     val showDetailArgs = viewModel.showDetailArg.observeAsState()
 
@@ -42,85 +53,166 @@ fun ShowDetailScreen(viewModel: ShowDetailViewModel = hiltViewModel()) {
                 .fillMaxWidth()
                 .verticalScroll(scrollState)
         ) {
-            (showDetailArgs.value?.showBackgroundUrl ?: showDetailArgs.value?.showImageUrl)?.let {
-                PosterImage(
-                    url = it,
-                    height = dimensionResource(id = R.dimen.show_backdrop_height)
-                )
-            } ?: run {
-                showSummary.value?.originalImageUrl?.let {
-                    PosterImage(
-                        url = it,
-                        height = dimensionResource(id = R.dimen.show_backdrop_height)
-                    )
-                }
-            }
-
-            showSummary.value?.name?.let { name ->
-                Text(
-                    text = name,
-                    style = MaterialTheme.typography.h5,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(
-                            start = 16.dp,
-                            top = 8.dp,
-                            end = 16.dp,
-                            bottom = 4.dp
-                        )
-                )
-            }
-
-            showSummary.value?.status?.let { status ->
-                Text(
-                    text = status,
-                    style = MaterialTheme.typography.body2,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(
-                            start = 16.dp,
-                            top = 8.dp,
-                            end = 16.dp,
-                            bottom = 4.dp
-                        )
-                )
-            }
-
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                showSummary.value?.originalImageUrl?.let {
-                    PosterImage(
-                        url = it,
-                        modifier = Modifier
-                            .width(dimensionResource(id = R.dimen.detail_poster_width))
-                            .height(
-                                dimensionResource(id = R.dimen.show_detail_poster_height)
-                            )
-                    )
-                }
-                Column() {
-                    showSummary.value?.airDays?.let {
-                        TitleAndHeadingText(
-                            title = it, heading = stringResource(
-                                id = R.string.show_detail_air_days_heading
-                            )
-                        )
-                    }
-
-                }
-            }
+            BackdropAndTitle(
+                showDetailArgs = showDetailArgs.value,
+                showSummary = showSummary.value
+            )
+            PosterAndMetadata(showSummary = showSummary.value)
             showSummary.value?.summary?.let {
                 Text(
-                    text = it,
-                    modifier = Modifier.fillMaxWidth(),
-                    style = MaterialTheme.typography.h6
+                    text = Jsoup.parse(it).text(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            start = 16.dp,
+                            top = 4.dp,
+                            end = 16.dp,
+                        ),
+                    style = MaterialTheme.typography.body2
                 )
             }
+            Buttons()
+            showCast.value?.let { ShowCastList(it) }
         }
+    }
+}
+
+@Composable
+fun BackdropAndTitle(showDetailArgs: ShowDetailArg?, showSummary: ShowDetailSummary?) {
+    (showDetailArgs?.showBackgroundUrl ?: showDetailArgs?.showImageUrl)?.let {
+        PosterImage(
+            url = it,
+            height = dimensionResource(id = R.dimen.show_backdrop_height)
+        )
+    } ?: run {
+        showSummary?.originalImageUrl?.let {
+            PosterImage(
+                url = it,
+                height = dimensionResource(id = R.dimen.show_backdrop_height)
+            )
+        }
+    }
+
+    showSummary?.name?.let { name ->
+        Text(
+            text = name,
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = 16.dp,
+                    top = 8.dp,
+                    end = 16.dp,
+                )
+        )
+    }
+
+    showSummary?.status?.let { status ->
+        Text(
+            text = status,
+            style = MaterialTheme.typography.body2,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = 16.dp,
+                    top = 4.dp,
+                    end = 16.dp,
+                    bottom = 4.dp
+                )
+        )
+    }
+}
+
+@Composable
+fun PosterAndMetadata(showSummary: ShowDetailSummary?) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.Top
+    ) {
+        showSummary?.originalImageUrl?.let {
+            PosterImage(
+                url = it,
+                modifier = Modifier
+                    .width(dimensionResource(id = R.dimen.compose_detail_poster_width))
+                    .height(
+                        dimensionResource(id = R.dimen.compose_show_detail_poster_height)
+                    )
+            )
+        }
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            showSummary?.airDays?.let {
+                TitleAndHeadingText(
+                    title = it, heading = stringResource(
+                        id = R.string.show_detail_air_days_heading
+                    )
+                )
+            }
+
+            showSummary?.genres?.let {
+                TitleAndHeadingText(
+                    title = it, heading = stringResource(
+                        id = R.string.show_detail_genres_heading
+                    )
+                )
+            }
+
+            Text(
+                text = stringResource(id = R.string.tv_maze_creative_commons_attribution_text_multiple),
+                style = MaterialTheme.typography.caption,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun Buttons() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Button(
+            onClick = {},
+            modifier = Modifier.padding(4.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.btn_show_detail_remove_from_favorites)
+            )
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier.padding(4.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.btn_show_detail_seasons)
+            )
+        }
+    }
+}
+
+@Composable
+fun ShowCastList(list: List<ShowCast>) {
+    LazyRow {
+        items(list) {
+            ShowCastCard(item = it)
+        }
+    }
+}
+
+@Composable
+fun ShowCastCard(item: ShowCast) {
+    Card {
+        item.name?.let { Text(text = it) }
     }
 }
 
@@ -129,9 +221,18 @@ fun TitleAndHeadingText(
     title: String,
     heading: String
 ) {
-    Column() {
-        Text(text = heading)
-        Text(text = title)
+    Column(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Text(
+            text = heading.uppercase(),
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.caption
+        )
+        Text(
+            text = title,
+            style = MaterialTheme.typography.body2
+        )
     }
 
 }
