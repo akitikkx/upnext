cab22bd4fae37dc9d55279edd9c12571bc3b7700
Ahmed Tikiwa <akitikkx@gmail.com>
2024-03-10 18:07:51 +0100

    Changed the ExistingPeriodicWorkPolicy for all the workers from UPDATE to CANCEL_AND_REENQUEUE as the both workers for the dashboard and explore were not both being run only one of them

--- app/src/main/java/com/theupnextapp/UpnextApplication.kt
+++ app/src/main/java/com/theupnextapp/UpnextApplication.kt
@@ -126,7 +126,7 @@ class UpnextApplication : Application(), Configuration.Provider {
 
                     workManager.enqueueUniquePeriodicWork(
                         RefreshFavoriteShowsWorker.WORK_NAME,
-                        ExistingPeriodicWorkPolicy.UPDATE,
+                        ExistingPeriodicWorkPolicy.CANCEL_AND_REENQUEUE,
                         refreshFavoriteShowsRequest
                     )
                 }
@@ -145,7 +145,7 @@ class UpnextApplication : Application(), Configuration.Provider {
 
         workManager.enqueueUniquePeriodicWork(
             RefreshTraktExploreWorker.WORK_NAME,
-            ExistingPeriodicWorkPolicy.UPDATE,
+            ExistingPeriodicWorkPolicy.CANCEL_AND_REENQUEUE,
             refreshExploreShowsRequest
         )
     }
@@ -161,7 +161,7 @@ class UpnextApplication : Application(), Configuration.Provider {
 
         workManager.enqueueUniquePeriodicWork(
             RefreshDashboardShowsWorker.WORK_NAME,
-            ExistingPeriodicWorkPolicy.UPDATE,
+            ExistingPeriodicWorkPolicy.CANCEL_AND_REENQUEUE,
             refreshDashboardShowsRequest
         )
     }
