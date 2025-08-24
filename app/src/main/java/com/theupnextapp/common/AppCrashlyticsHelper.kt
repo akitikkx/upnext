d07a2e0215295418dd4485749dda0e0083aec149
Ahmed Tikiwa <akitikkx@gmail.com>
2025-08-24 19:29:31 +0200

    Implement Crashlytics helper and Hilt module

---
+++ app/src/main/java/com/theupnextapp/common/AppCrashlyticsHelper.kt
@@ -0,0 +1,15 @@
+package com.theupnextapp.common
+
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import javax.inject.Inject
+
+class AppCrashlyticsHelper @Inject constructor() : CrashlyticsHelper {
+
+    override fun log(message: String) {
+        FirebaseCrashlytics.getInstance().log(message)
+    }
+
+    override fun recordException(e: Throwable) {
+        FirebaseCrashlytics.getInstance().recordException(e)
+    }
+}
