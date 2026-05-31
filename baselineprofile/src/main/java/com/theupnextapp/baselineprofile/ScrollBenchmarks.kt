9e1e0f6e7922b6ce7d21dca9d57b603b5fa358de
Ahmed Tikiwa <akitikkx@gmail.com>
2026-05-31 22:29:27 +0200

    chore(benchmark): enhance scroll benchmarks and baseline profiles with TraceSectionMetric

--- baselineprofile/src/main/java/com/theupnextapp/baselineprofile/ScrollBenchmarks.kt
+++ baselineprofile/src/main/java/com/theupnextapp/baselineprofile/ScrollBenchmarks.kt
@@ -13,8 +13,10 @@
 package com.theupnextapp.baselineprofile
 
 import androidx.benchmark.macro.CompilationMode
+import androidx.benchmark.macro.ExperimentalMetricApi
 import androidx.benchmark.macro.FrameTimingMetric
 import androidx.benchmark.macro.StartupMode
+import androidx.benchmark.macro.TraceSectionMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -22,6 +24,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+@OptIn(ExperimentalMetricApi::class)
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class ScrollBenchmarks {
@@ -38,7 +41,7 @@ class ScrollBenchmarks {
     private fun scroll(compilationMode: CompilationMode) {
         rule.measureRepeated(
             packageName = "com.theupnextapp",
-            metrics = listOf(FrameTimingMetric()),
+            metrics = listOf(FrameTimingMetric(), TraceSectionMetric("TMDBImageFetch")),
             compilationMode = compilationMode,
             iterations = 10,
             startupMode = StartupMode.WARM,
